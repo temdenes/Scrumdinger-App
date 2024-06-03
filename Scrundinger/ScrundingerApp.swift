fc9033dc24c73d1ed51caddfe209c58c874377f0
Dénes Temleitner <110195640+temdenes@users.noreply.github.com>
2024-06-03 19:18:06 +0200

    Initial Commit

---
+++ Scrundinger/ScrundingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrundingerApp.swift
+//  Scrundinger
+//
+//  Created by Temleitner Dénes on 03/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrundingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
