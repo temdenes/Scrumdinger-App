3df019bf90f4ff87ed139f52fb6fc1a788d36641
Dénes Temleitner <110195640+temdenes@users.noreply.github.com>
2024-06-10 18:37:49 +0200

    End of Apple Dev Course this part: Persistence and concurrency Persisting data

--- Scrundinger/Models/History.swift
+++ Scrundinger/Models/History.swift
@@ -7,11 +7,11 @@
 
 import Foundation
 
-struct History: Identifiable {
+struct History: Identifiable, Codable {
     let id: UUID
     let date: Date
     var attendees: [DailyScrum.Attendee]
-    
+
     init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee]) {
         self.id = id
         self.date = date
