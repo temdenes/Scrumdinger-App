eb2d6b92fee68a083575eebdf1b29d541789ed08
Dénes Temleitner <110195640+temdenes@users.noreply.github.com>
2024-06-11 00:13:10 +0200

    End Apple App Dev Tutorial this part: Drawing Drawing the timer view

---
+++ Scrundinger/Views/MeetingTimerView.swift
@@ -0,0 +1,18 @@
+//
+//  MeetingTimerView.swift
+//  Scrundinger
+//
+//  Created by Temleitner Dénes on 10/06/2024.
+//
+
+import SwiftUI
+
+struct MeetingTimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeetingTimerView()
+}
---
+++ Scrundinger/Views/SpeakerArc.swift
@@ -0,0 +1,18 @@
+//
+//  SpeakerArc.swift
+//  Scrundinger
+//
+//  Created by Temleitner Dénes on 10/06/2024.
+//
+
+import SwiftUI
+
+struct SpeakerArc: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpeakerArc()
+}
